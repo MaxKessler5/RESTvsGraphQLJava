6457f4700a69befcdd9aebd184917a1650053913
Max <mkessler@gmu.edu>
2025-04-10 19:21:40 -0600

    working java with gradle in vs code

---
+++ src/main/java/mycool/stuff/hello.java
@@ -0,0 +1,16 @@
+package mycool.stuff;
+
+import okhttp3.*;
+import org.json.JSONObject;
+
+public class hello {
+    public static void main(String[] args) {
+
+        System.out.println("Runtime Java Version:");
+        System.out.println("  java.version:      " + System.getProperty("java.version"));
+        System.out.println("  java.vendor:       " + System.getProperty("java.vendor"));
+        System.out.println("  java.home:         " + System.getProperty("java.home"));
+        System.out.println("  java.class.version: " + System.getProperty("java.class.version"));
+        
+    }
+}
